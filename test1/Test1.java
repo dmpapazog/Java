63a14368a64c9190b33b90d954fa85af33973575
dmpapazog <dmpapazog@gmail.com>
2019-01-21 16:47:42 +0200

    test1 changes

--- test1/Test1.java
+++ test1/Test1.java
@@ -1,4 +1,7 @@
 public class Test1 {
+    /** main function
+     * @param args The command line {@code String} parameters
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
